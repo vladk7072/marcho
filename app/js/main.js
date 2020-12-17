06580910eba3ca6074fa8274b2c0962047497715
vladk7072 <vladk7072@gmail.com>
2020-12-17 13:11:20 +0200

    added contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -1,70 +1,76 @@
-$(function(){
-
-  $('.product-tabs__top-item').on('click', function(e){
-    e.preventDefault();
-    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
-    $(this).addClass('product-tabs__top-item--active');
-
-    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
-    $($(this).attr('href')).addClass('product-tabs__content-item--active');
-    
-  });
+$(function () {
   
-  $('.product-slide__thumb').slick({
-    asNavFor: '.product-slide__big',
+
+  $(".product-tabs__top-item").on("click", function (e) {
+    e.preventDefault();
+    $(".product-tabs__top-item").removeClass("product-tabs__top-item--active");
+    $(this).addClass("product-tabs__top-item--active");
+
+    $(".product-tabs__content-item").removeClass(
+      "product-tabs__content-item--active"
+    );
+    $($(this).attr("href")).addClass("product-tabs__content-item--active");
+  });
+
+  $(".product-slide__thumb").slick({
+    asNavFor: ".product-slide__big",
     focusOnSelect: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     vertical: true,
-    draggable: false
+    draggable: false,
   });
-  $('.product-slide__big').slick({
-    asNavFor: '.product-slide__thumb',
+  $(".product-slide__big").slick({
+    asNavFor: ".product-slide__thumb",
     draggable: false,
     arrows: false,
-    fade: true
+    fade: true,
   });
 
-  $('.shop-content__filter-btn').on('click', function(){
-    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
-    $(this).addClass('shop-content__filter-btn--active');
+  $(".shop-content__filter-btn").on("click", function () {
+    $(".shop-content__filter-btn").removeClass(
+      "shop-content__filter-btn--active"
+    );
+    $(this).addClass("shop-content__filter-btn--active");
   });
 
-  $('.button-list').on('click', function(){
-    $('.product-item').addClass('product-item--list');
+  $(".button-list").on("click", function () {
+    $(".product-item").addClass("product-item--list");
   });
-  $('.button-grid').on('click', function(){
-    $('.product-item').removeClass('product-item--list');
+  $(".button-grid").on("click", function () {
+    $(".product-item").removeClass("product-item--list");
   });
 
-  $('.select-style, .product-one__item-num').styler();
+  $(".select-style, .product-one__item-num").styler();
 
-  $('.filter-price__input').ionRangeSlider({
+  $(".filter-price__input").ionRangeSlider({
     type: "double",
     prefix: "$",
-    onStart: function(data){
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
+    onStart: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
     },
     onChange: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
     },
   });
 
-  $('.top-slider__inner').slick({
+  $(".top-slider__inner").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
   $(".star").rateYo({
     starWidth: "17px",
     normalFill: "#ccccce",
     rateFill: "ffc35b",
-    readOnly: true
+    readOnly: true,
+    starSvg:
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18pt" height="16pt" viewBox="0 0 18 16" version="1.1"><g id="surface1"><path style=" stroke:none;"d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156 L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812 C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438 6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406 8.101562 0.554688 Z M 8.101562 0.554688 "/></g></svg>',
   });
 
   function getTimeRemaining(endtime) {
@@ -73,41 +79,43 @@ $(function(){
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
       hours,
       minutes,
-      seconds
+      seconds,
     };
   }
-  
+
   function initializeClock(id, endtime) {
-    const clock = document.querySelector('.promo__clock');
-    const daysSpan = clock.querySelector('.promo__days');
-    const hoursSpan = clock.querySelector('.promo__hours');
-    const minutesSpan = clock.querySelector('.promo__minutes');
-    const secondsSpan = clock.querySelector('.promo__seconds');
-  
+    const clock = document.querySelector(".promo__clock");
+    const daysSpan = clock.querySelector(".promo__days");
+    const hoursSpan = clock.querySelector(".promo__hours");
+    const minutesSpan = clock.querySelector(".promo__minutes");
+    const secondsSpan = clock.querySelector(".promo__seconds");
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+      hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
   }
+
+  const deadline = $(".promo__clock").attr("data-time");
+  initializeClock("promo__clock", deadline);
+
   
-  const deadline = $('.promo__clock').attr('data-time');
-  initializeClock('promo__clock', deadline);
 
 });
